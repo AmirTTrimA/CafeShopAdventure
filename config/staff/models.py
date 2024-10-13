dcd393ed5ddd49e3b55beb569c1699e3b93c50db
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-10-13 04:57:34 -0700

    models.py

--- config/staff/models.py
+++ config/staff/models.py
@@ -1,45 +1,33 @@
-"""
-models.py
-
-This module defines the Staff model for the staff application,
-including attributes for staff members.
-"""
-
 from django.db import models
 
 
+
 class Staff(models.Model):
-    """
-    Model representing a staff member.
+    ROLE_CHOICES = (
+        ('Manager', 'Manager'),
+        ('Waiter', 'Waiter'),
+        ('Chef', 'Chef'),
+        ('assistant cook', 'assistant cook'),
+        ('dish washer','dish washer'),
+        ('Services','Services')
 
-    Attributes:
-        staff_id (int): The primary key for the staff member (auto-incremented).
-        first_name (str): The first name of the staff member.
-        last_name (str): The last name of the staff member.
-        email (str): The unique email address of the staff member.
-        password (str): The password of the staff member (should be hashed).
-        role (str): The role of the staff member.
-        create_at (datetime): The timestamp when the staff member was created.
-        update_at (datetime): The timestamp when the staff member was last updated.
-    """
 
-    staff_id = models.AutoField(primary_key=True)
+    )
+    staff_id=models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(
-        max_length=128
-    )  # Password should be hashed; max length should be at least 128 chars
-    role = models.CharField(
-        max_length=40
-    )  # For roles, create a class and use choices in Django
-    create_at = models.DateTimeField(
-        auto_now_add=True
-    )  # Automatically set the field to now when the object is created
-    update_at = models.DateTimeField(
-        auto_now=True
-    )  # Automatically set the field to now every time the object is saved
+    email = models.EmailField(max_length=100,unique=True)
+    password = models.CharField(max_length=128)
+    role = models.CharField(max_length=40,choices=ROLE_CHOICES)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    # relation_st_or=models.ForeignKey(Order,on_delete=models.CASCADE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
 
     def __str__(self):
-        """Return a string representation of the staff member."""
         return f"{self.first_name} {self.last_name}"
+
+
