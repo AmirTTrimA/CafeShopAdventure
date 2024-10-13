049a4f84c375a9fc9076d5bb4b56d44bc6a0830d
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-10-13 05:25:34 -0700

    models.py

--- config/staff/models.py
+++ config/staff/models.py
@@ -1,15 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
+class StaffManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('Staff must have an email address')
+        email = self.normalize_email(email)
+        staff = self.model(email=email, first_name=first_name, last_name=last_name)
+        staff.set_password(password)  
+        staff.save(using=self._db)
+        return staff
 
-class Staff(models.Model):
+    def create_superuser(self, email, first_name, last_name, password=None):
+        staff = self.create_user(email, first_name, last_name, password)
+        staff.is_admin = True
+        staff.is_superuser = True
+        staff.save(using=self._db)
+        return staff
+    
+class Staff(models.Model,AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
-        ('Manager', 'Manager'),
-        ('Waiter', 'Waiter'),
-        ('Chef', 'Chef'),
+        ('manager', 'manager'),
+        ('waiter', 'waiter'),
+        ('chef', 'chef'),
         ('assistant cook', 'assistant cook'),
         ('dish washer','dish washer'),
-        ('Services','Services')
+        ('services','services')
 
 
     )
@@ -23,11 +40,17 @@ class Staff(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     # relation_st_or=models.ForeignKey(Order,on_delete=models.CASCADE)
 
+    objects = StaffManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+    @property
+    def is_admin(self):
+        return self.is_superuser
 
 
