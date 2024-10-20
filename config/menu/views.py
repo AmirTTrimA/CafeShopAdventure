8f1d77f4bc7860224f53f6a5c94b656a96c7aa6f
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-10-20 07:11:03 -0700

    views.py

--- config/menu/views.py
+++ config/menu/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import MenuItem,Category
 
-# Create your views here.
+def menu(request):
+    products = MenuItem.objects.all().values()
+    category=Category.objects.all().values()
+    return render(request, 'menu\menu.html', {'product':products,'category':category})
+
+def product_detail(request,product_id):
+    queryset=MenuItem.objects.filter(id=product_id)
+    product = get_object_or_404(queryset)
+    context = {
+        'product': product
+    }
+    return render(request, 'menu\product.html', context)
