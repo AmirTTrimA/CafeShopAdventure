6b0353fae49a7023b037ee90035827bda1edaddb
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-10-20 07:12:17 -0700

    urls.py

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from menu import views as views_menu
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("staff/", include('staff.urls')),
-    path("order/", include('order.urls')),
+    # path("order/",views_menu.product_detail, name='product.html' ),
+    path("menu/",views_menu.menu, name='product' ),
+    # path("menu/product.html",views_menu.product_detail, name='product' ),
+    path('menu/product/<int:product_id>', views_menu.product_detail, name='product')
 ]
