4e9a4e628b98b617512011de9560034e1e835a7a
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-10-22 01:36:26 -0700

    Add files via upload

---
+++ config/menu/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ProductDetailView,CafeMenuView
+
+urlpatterns = [path("", CafeMenuView.as_view(), name="menu"),
+               path('product/<int:pk>', ProductDetailView.as_view(), name='product'),]
+
