93d5de189d5c05e8106a2871ac1713010da099de
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-11-04 02:04:10 -0800

    backends.py

--- config/staff/backends.py
+++ config/staff/backends.py
@@ -3,7 +3,26 @@ from django.contrib.auth.backends import ModelBackend
 from .models import Staff
 
 class PhoneNumberBackend(ModelBackend):
+    """
+    Custom authentication backend that authenticates users based on their phone number.
+    Attributes:
+        model (Model): The model class to use for authentication, which should be a subclass of 
+        Django's `User` model (in this case, `Staff`).
+    """
     def authenticate(self, request, phone_number=None, password=None, **kwargs):
+        """
+        Authenticate a user based on the provided phone number and password.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+            phone_number (str): The phone number of the user attempting to log in.
+            password (str): The password of the user attempting to log in.
+            **kwargs: Additional arguments that may be passed (unused in this method).
+
+        Returns:
+            User or None: If authentication is successful, returns the authenticated user
+            instance. If authentication fails or the user does not exist, returns None.
+        """
         try:
             user = Staff.objects.get(phone_number=phone_number)
         except Staff.DoesNotExist:
