5f26e26b1750e6f65b5de406913aafed233209ec
Amirttrima <amirsrst@gmail.com>
2024-11-05 12:27:26 +0330

    analytics app deleted

--- config/analytics/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('customer-analytics/', views.CustomerAnalyticsView.as_view() , name='customer-analytics'),
-    path('orders/', views.OrdersView.as_view() , name='orders'),
-
-]
